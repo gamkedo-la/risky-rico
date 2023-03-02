bfec71e1aa00b9685b68ce9f13f9b69fe1228b2b
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-03-01 18:03:04 -0600

    display score as TMP text

---
+++ Assets/UI/UpdateText.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ScriptableObjectArchitecture;
+using TMPro;
+
+public class UpdateText : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private FloatVariable _value;
+    void Update()
+    {
+        _text.text = "Score: " + _value.Value;
+    }
+}
