3984bd01d7d2b51cf87f8f7f160d4b775babd83b
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-03-04 21:22:54 -0600

    Update Todo.cs

--- Assets/Todo.cs
+++ Assets/Todo.cs
@@ -3,11 +3,6 @@ using UnityEngine;
 public class Todo : MonoBehaviour
 {
     // TODO: spawn waves:
-    // timer for wave duration    
-    // timer for rest duration    
-    // dictionary mapping enemies to score milestones (add them to pool of spawnable enemies)    
-    // change spawnable list to SO    
-    // SO var for time between spawns (ALT: only spawn on enemy destroy?)
     // timeBetweenSpawns is the "tempo" of the game - starts slow, gets faster, slows down, then ramps up near the end of a wave
     // -- split waves into "quarters" for these fast/slow spawn rates (use dictionary to map tem)
 }
