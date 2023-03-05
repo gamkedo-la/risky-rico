47ec7668875c6b62e3d1e0518863c3fd51353942
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-03-04 21:39:41 -0600

    Update Todo.cs

--- Assets/Todo.cs
+++ Assets/Todo.cs
@@ -2,7 +2,5 @@ using UnityEngine;
 
 public class Todo : MonoBehaviour
 {
-    // TODO: spawn waves:
-    // timeBetweenSpawns is the "tempo" of the game - starts slow, gets faster, slows down, then ramps up near the end of a wave
-    // -- split waves into "quarters" for these fast/slow spawn rates (use dictionary to map tem)
+
 }
