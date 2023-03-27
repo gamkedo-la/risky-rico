db2331c3617ac969b2ed24abba35590e484d743b
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-03-27 08:45:07 -0500

    todo list

--- Assets/Todo.cs
+++ Assets/Todo.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class Todo : MonoBehaviour
 {
-
+    // TODO: refactor existing code [FIRST] (less rigidbodies, more raycasts, leverage SO vars, etc)
+    // TODO: play jam version
+    // TODO: review JS code
+    // TODO: hitbox favortism
+    // TODO: combo-based score system (points for hitting an enemy, hitting wall breaks combo)
+    // TODO: lines of enemies coming from one direction?
+    // TODO: balancing (speed, tempo, spawn types)
+    // TODO: give each enemy type its own mini-wave
+    // TODO: switch to custom wave design via SOs?
+    // TODO: rotating enemy
+    // TODO: highscore recording
+    // TODO: basic options
 }
