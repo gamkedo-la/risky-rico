d7fd393c533363b2e943139ade8265b03dddcc90
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-04-10 22:43:00 -0500

    increasing combo score multiplier for consecutive kills; resetting on combo break

--- Assets/Scoring/UpdateScore.cs
+++ Assets/Scoring/UpdateScore.cs
@@ -4,14 +4,25 @@ using ScriptableObjectArchitecture;
 public class UpdateScore : MonoBehaviour
 {
     [SerializeField] private FloatVariable _score;
+    [SerializeField] private FloatVariable _scoreMultiplier;
+    [SerializeField] private FloatVariable _maxScoreMultiplier;
 
     void Awake()
     {
         _score.Value = 0f;
+        _scoreMultiplier.Value = 1f;
+    }
+
+    public void BreakCombo()
+    {
+        _scoreMultiplier.Value = 1f;
     }
 
     public void IncreaseScore()
     {
-        _score.Value += 10;
+        // increase the score multiplier for consecutive kills; don't increase it past the max multiplier
+        _score.Value += 10 * _scoreMultiplier.Value;
+        _scoreMultiplier.Value += 1f;
+        _scoreMultiplier.Value = Mathf.Clamp(_scoreMultiplier.Value, 0f, _maxScoreMultiplier.Value);
     }
 }
