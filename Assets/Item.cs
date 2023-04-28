57753b8c892e85b0bfb6202e5b431c3f2b0cc712
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-04-27 22:11:58 -0500

    script for item prefab

---
+++ Assets/Item.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private ItemParameters _parameters;
+    [SerializeField] private SpriteRenderer _renderer;
+
+    void Awake()
+    {
+        // get necesssary components
+        _renderer = gameObject.GetComponent<SpriteRenderer>();
+
+        // apply parameters to individual components
+        SetParameters(_parameters);
+    }
+
+    public void SetParameters(ItemParameters parameters)
+    {
+        _parameters = parameters;
+        _renderer.sprite = _parameters.Image;
+    }
+}
