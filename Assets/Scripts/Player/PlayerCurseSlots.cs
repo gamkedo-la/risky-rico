088583784c28b3c83c6481444c469c2777f7a3c0
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-04-29 21:41:20 -0500

    player curse slots

---
+++ Assets/Scripts/Player/PlayerCurseSlots.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class PlayerCurseSlots : MonoBehaviour
+{
+    [Header("Slots")]
+    [SerializeField] private List<CurseParameters> _curses = new List<CurseParameters>();
+    [SerializeField] private int _maxSlotCount = 3;
+    
+    [Header("Player")]
+    [SerializeField] private PlayerParameters _player;
+
+    private void ActivateCurse(CurseParameters curse)
+    {
+        _player.ApplyCurse(curse);
+    }
+
+    private void DeactivateCurse(CurseParameters curse)
+    {
+        _player.RemoveCurse(curse);
+    }
+
+    public void RemoveCurse(CurseParameters curse)
+    {
+        if (_curses.Contains(curse))
+        {
+            _curses.Remove(curse);
+            DeactivateCurse(curse);
+        }
+    }
+
+    public void AddCurse(CurseParameters curse)
+    {
+        if (!_curses.Contains(curse) && _curses.Count < _maxSlotCount)
+        {
+            _curses.Add(curse);
+            ActivateCurse(curse);
+        }
+    }
+}
