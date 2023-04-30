391a78826604aad63abdef9b090cb20c50e884ed
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-04-29 19:11:53 -0500

    removing rayrange struct from playerextras

--- Assets/Scripts/Player/PlayerExtras.cs
+++ Assets/Scripts/Player/PlayerExtras.cs
@@ -15,13 +15,3 @@ public interface IPlayerController {
         Vector3 RawMovement { get; }
         bool Grounded { get; }
 }
-
-public struct RayRange {
-    public RayRange(float x1, float y1, float x2, float y2, Vector2 dir) {
-        Start = new Vector2(x1, y1);
-        End = new Vector2(x2, y2);
-        Dir = dir;
-    }
-
-    public readonly Vector2 Start, End, Dir;
-}
