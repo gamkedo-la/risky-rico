30111a21db368f180c85caf27baa29303132423e
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-04-29 23:41:27 -0500

    ammo sprite

--- Assets/Scripts/Todo.cs
+++ Assets/Scripts/Todo.cs
@@ -4,7 +4,6 @@ public class Todo : MonoBehaviour
 {
     // FOR COMBAT LOOP:
     // TODO: refactor existing code [FIRST] (more raycasts, leverage SO vars, etc)
-    // TODO: create sample item sprites
     // TODO: signify end of waves with a timer bar
     // TODO: signify end of combat with text and effects
     // TODO: loop first level for testing
