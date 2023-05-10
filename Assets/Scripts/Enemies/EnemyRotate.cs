5e748a964e916d0e6c01e91a9db2e1ef7cfccea7
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-10 17:11:04 -0500

    script to rotate enemies periodically

---
+++ Assets/Scripts/Enemies/EnemyRotate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotate : MonoBehaviour
+{
+   [SerializeField] public EnemyParameters enemy;
+   private float _rotationTimer;
+
+   void Update()
+   {
+        _rotationTimer += enemy.RotationFrequency * Time.deltaTime;
+
+        if (enemy.RotationFrequency > 0f &&  _rotationTimer >= 1f)
+        {
+            // rotate the enemy gameobject
+            transform.Rotate(0,0,90);
+
+            // reset the timer
+            _rotationTimer = 0f;
+        }
+   }
+}
