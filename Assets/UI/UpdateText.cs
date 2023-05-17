eff0204a7e52f48f624bdbde11eb183b4838add4
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-17 18:14:20 -0500

    adding support for string references in UpdateText

--- Assets/UI/UpdateText.cs
+++ Assets/UI/UpdateText.cs
@@ -9,9 +9,25 @@ public class UpdateText : MonoBehaviour
 {
 
     [SerializeField] private TMP_Text _text;
-    [SerializeField] private FloatVariable _value;
+    
+    [Header("FLOAT VALUE")]
+    [SerializeField] private bool _useFloat = false;
+    [SerializeField, HideCustomDrawer] private FloatVariable _value;
+
+    [Header("STRING VALUE")]
+    [SerializeField] private bool _useString = false;
+    [SerializeField, HideCustomDrawer] private StringReference _stringValue;
+
     void Update()
     {
-        _text.text = "" + _value.Value;
+        if (_useFloat && _value != null)
+        {
+            _text.text = "" + _value.Value;
+        }
+
+        if (_useString && _stringValue != null)
+        {
+            _text.text = _stringValue.Value;
+        }
     }
 }
