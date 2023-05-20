083d20211b320b2ef805297dcf8ddb6ebc2302c2
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-20 16:34:10 -0500

    todo list for UI implementation

--- Assets/Scripts/Todo.cs
+++ Assets/Scripts/Todo.cs
@@ -13,4 +13,20 @@ public class Todo : MonoBehaviour
     // TODO: basic options
     // TODO: scoring zones based on proximity to player (closer to player, higher score)
     // TODO: consider global attribute set?
+
+    // UI:
+    // TODO: menu prefab
+    // TODO: --- header
+    // TODO: --- selection cursor prefab
+    // TODO: --- input list
+    // TODO: --- slider with label prefab
+    // TODO: --- selection with label prefab
+    // TODO: --- explanation text object
+    // TODO: --- option data: (explanation text, label, type, value, handler)
+    // TODO: --- input handler (move cursor, select options, back out of menu)
+    // TODO: menu stack
+    // TODO: options menu prefab
+    // TODO: main menu prefab
+    // TODO: pause menu prefab
+    // TODO: confirm menu prefab (special input handler)
 }
