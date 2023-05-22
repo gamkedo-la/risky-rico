ca3bc4687d70906994582b97aa25998f9a2593e2
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-21 21:02:13 -0500

    converting main menu and options menu to prefabs

--- Assets/Scripts/Todo.cs
+++ Assets/Scripts/Todo.cs
@@ -3,24 +3,16 @@ using UnityEngine;
 public class Todo : MonoBehaviour
 {
     // FOR COMBAT LOOP:
-    // TODO: refactor existing code [FIRST] (more raycasts, leverage SO vars, etc)
+    // TODO: refactor existing code (consitent style, use custom collider)
     // TODO: loop first level for testing
     
     // GENERAL:
     // TODO: hitbox favortism
     // TODO: balancing (speed, tempo, spawn types)
     // TODO: highscore recording
-    // TODO: basic options
     // TODO: scoring zones based on proximity to player (closer to player, higher score)
     // TODO: consider global attribute set?
 
     // UI:
-    // TODO: menu prefab
-    // TODO: --- input handler 
-
-    // TODO: menu stack (back out of menu with spacebar)
-    // TODO: options menu prefab
-    // TODO: main menu prefab
-    // TODO: pause menu prefab
     // TODO: confirm menu prefab (special input handler)
 }
