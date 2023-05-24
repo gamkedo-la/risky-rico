9191a53d19ffb95378608efbdde798b8bdf5274a
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-23 20:05:13 -0500

    boot strapping services

---
+++ Assets/Scripts/Services/Bootstrapper.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+
+public static class Bootstrapper
+{
+    /// <summary>
+    /// This function is used to initialize a variety of services and setup the initial state of the game.
+    /// </summary>
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Initialize()
+    {
+        ServiceLocator.Initialize();
+        
+        //Setup services that must be attached to a GameObject
+        GameObject singletonObject = new GameObject("singletonObject",
+            typeof(EventSystem),
+            typeof(InputSystemUIInputModule),
+            typeof(MonoBehaviorService));
+        ServiceLocator.Instance.Register(singletonObject.GetComponent<MonoBehaviorService>());
+        Object.DontDestroyOnLoad(singletonObject);
+        
+        //Setup Services
+        ServiceLocator.Instance.Register(new AudioManager());
+    }
+}
+
+
