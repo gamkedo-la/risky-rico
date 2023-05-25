a0160f61fcea14ab641acdd25ae1982513f96bfd
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-24 20:20:38 -0500

    naive camera tracking in hub areas

---
+++ Assets/Scripts/Camera/CameraTracking.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    [SerializeField] private GameObject _focalPoint;
+    [SerializeField] private float _trackingSpeed = 0.01f;
+
+    void Start()
+    {
+        if (_focalPoint != null)
+        {
+            SetPositionToFocalPoint();
+        }
+    }
+
+    void Update()
+    {
+        SetPositionToFocalPoint();
+    }
+
+    void SetPositionToFocalPoint()
+    {
+        Vector3 focalPointPosition = new Vector3(_focalPoint.transform.position.x, _focalPoint.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, focalPointPosition, _trackingSpeed);
+    }
+}
