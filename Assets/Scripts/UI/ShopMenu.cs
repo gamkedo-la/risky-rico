1dde61b736d480bc85785ff1b75e0d8260ac18b5
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-25 20:49:25 -0500

    script for displaying selected shop item details

---
+++ Assets/Scripts/UI/ShopMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShopMenu : MonoBehaviour
+{
+    [SerializeField] private GridMenu _gridMenu;
+    [SerializeField] private TMP_Text _itemName;
+    [SerializeField] private TMP_Text _itemDescription;
+    [SerializeField] private TMP_Text _itemPrice;
+
+    void Update()
+    {
+        InputField currentInput = _gridMenu.GetActiveInput();
+
+        if (currentInput != null && currentInput is ShopItemField)
+        {
+            ShopItemField currentItem = (ShopItemField)currentInput;
+            ItemParameters item = currentItem.ItemData;
+            _itemName.text = item.Name;
+            _itemDescription.text = item.Description;
+            _itemPrice.text = "" + item.Price;
+        }
+    }
+
+    ShopItemField GetActiveItem()
+    {
+        ShopItemField currentItem = null;
+
+        foreach (ShopItemField field in _gridMenu.Fields)
+        {
+            if (field.InputEnabled)
+            {
+                currentItem = field;
+            }
+        }
+
+        return currentItem;
+    }
+}
