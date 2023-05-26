b3e5dd4d42e5d8489f50f6e14c69a129ee9a2d65
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-26 17:05:38 -0500

    updating dialog script with user input, space skipping, and invisible characters

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -2,31 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class Dialog : MonoBehaviour
 {
+    [Header("INPUT")]
+    [SerializeField] private PlayerInput _input;
+    
+    [Header("CONTENT")]
     public TextMeshProUGUI textDisplay;
     public string[] sentences;
     private int index;
-    public float typingSpeed;
 
+    [Header("SETTINGS")]
+    [Tooltip("Select a speed setting for the text typewriter (higher number = faster text)")]
+    [Range(1, 10)]
+    public int typingSpeed;
+
+    IEnumerator typingCoroutine;
     
-    // Start is called before the first frame update
     void Start()
     {
-        StartEnumerator(TypeWritter());
+        typingCoroutine = TypeWriter();
+        StartCoroutine(typingCoroutine);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        string currentSentence = sentences[index];
+
+        if (_input.actions["interact"].triggered && textDisplay.text != currentSentence)
+        {
+            StopCoroutine(typingCoroutine);
+            AutoCompleteSentence();
+        }
+        else if (_input.actions["interact"].triggered && textDisplay.text == currentSentence)
+        {
+            MoveToNextSentence();
+            typingCoroutine = TypeWriter();
+            StartCoroutine(typingCoroutine);
+        }
     }
 
+    IEnumerator TypeWriter()
+    {
+        string currentSentence = sentences[index];
+        char[] currentSentenceCharacters = currentSentence.ToCharArray();
+        int characterIndex = 0;
 
-    IEnumerator TypeWritter(){
-        foreach (char letter in sentences[index].ToCharArray()){
-            textDisplay.text += letter;
+        if (textDisplay.text != currentSentence)
+        {
+            while(characterIndex <= currentSentenceCharacters.Length - 1)
+            {
+                char currentCharacter = currentSentenceCharacters[characterIndex];
+                string textToDisplay = currentSentence.Substring(0, characterIndex + 1);
+
+                if (currentCharacter.ToString() == " " && characterIndex + 1 <= currentSentenceCharacters.Length - 1)
+                {
+                    textToDisplay += currentSentenceCharacters[characterIndex + 1];
+                    characterIndex += 1;
+                }
+
+                textToDisplay += "<color=#000000>" + currentSentence.Substring(characterIndex) + "</color>";
+
+                textDisplay.text = textToDisplay;
+
+                characterIndex += 1;
+
+                yield return new WaitForSeconds(1f/(float)typingSpeed);
+            }
         }
+    }
+
+    void AutoCompleteSentence()
+    {
+        if (textDisplay.text != sentences[index])
+        {
+            textDisplay.text = sentences[index];
+        }
+    }
+
+    void MoveToNextSentence()
+    {
+        index += 1;
+
+        if (index > sentences.Length - 1)
+        {
+            index = sentences.Length - 1;
+            return;
+        }
+    
+        textDisplay.text = "";
     }
 }
