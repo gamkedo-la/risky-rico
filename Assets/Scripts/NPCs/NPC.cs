a10ce21dc0e9b31f15b974cc4e2cc29326627566
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-29 15:17:42 -0500

    updating NPC prefabs with new scripts

---
+++ Assets/Scripts/NPCs/NPC.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using ScriptableObjectArchitecture;
+
+public class NPC : MonoBehaviour, IInteractable
+{
+    [Header("DIALOG")]
+    [SerializeField, HideCustomDrawer] private DialogSequence _activeDialogueSequence;
+    [SerializeField] private DialogSequence _npcDialogueSequence;
+    
+    [Header("EVENTS")]
+    [SerializeField] private GameEvent _onDialogStart;
+    [SerializeField] private UnityEvent _onDialogEnd;
+    
+    [Header("INTERACTIONS")]
+    [SerializeField] private string _interactionPrompt;
+
+    public string Prompt {get; set; }
+    public bool InteractionEnabled {get; set; }
+
+    void Start()
+    {
+        Prompt = _interactionPrompt;
+        InteractionEnabled = true;
+    }
+
+    public void ReceiveInteraction(GameObject interactor)
+    {
+       if (_npcDialogueSequence != null && _activeDialogueSequence != null)
+       {
+            _activeDialogueSequence.SetLines(_npcDialogueSequence.Lines);
+
+            _onDialogStart?.Raise();
+       }
+    }
+}
