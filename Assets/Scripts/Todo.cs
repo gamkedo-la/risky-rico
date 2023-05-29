7fbe4ff0142a8b2eb48926159e0ba994df11d748
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-29 12:17:43 -0500

    todo item for the shop

--- Assets/Scripts/Todo.cs
+++ Assets/Scripts/Todo.cs
@@ -17,4 +17,16 @@ public class Todo : MonoBehaviour
     // TODO: state machines (for animation, game state, player state, enemy state)
     // TODO: SO vars as middleman for UI
     // TODO: refactor existing code (consitent style, use custom collider, find anti-patterns)
+
+    // SHOP:
+    // TODO: create player inventory and ammo reserve (test with weapon types)
+    // TODO: buy items from shop (give player ammo, give player weapons)
+    // TODO: play cursor animation
+    // TODO: make shopkeeper interactable
+    // TODO: bring up dialogs when interacting with shopkeeper
+    // TODO: bring up shop menu when done talking with shopkeeper
+    // TODO: disable player movement/shooting when in shop menu
+    // TODO: write shopkeeper dialog (add flags for game progress)
+    // TODO: emit particles from shop chimney
+    // TODO: change particle texture after player buys things
 }
