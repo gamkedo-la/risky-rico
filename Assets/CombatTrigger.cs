c635730c41defa49445f8f171ed88f2fa02c14cb
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-06-10 09:57:21 -0500

    adding enemy spawn triggers to combat rooms

---
+++ Assets/CombatTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class CombatTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject _spawner;
+    [SerializeField] private GameObjectCollection _collidableObjects;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_collidableObjects.Contains(collision.gameObject))
+        {
+            _spawner.SetActive(true);
+        }
+    }
+}
