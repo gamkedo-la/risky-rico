b8094d8088a459c7a82457f176ca1692c2030e23
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-06-15 20:10:21 -0500

    updating level manager params

--- Assets/Scripts/Todo.cs
+++ Assets/Scripts/Todo.cs
@@ -7,7 +7,6 @@ public class Todo : MonoBehaviour
     // TODO: balancing (speed, tempo, spawn types)
     // TODO: highscore recording
     // TODO: scoring zones based on proximity to player (closer to player, higher score)
-    // TODO: consider global attribute set?
 
     // CODE PATTERNS:
     // TODO: input service
@@ -20,4 +19,12 @@ public class Todo : MonoBehaviour
     // TODO: emit particles from shop chimney
     // TODO: change particle texture after player buys things
     // TODO: play cursor sound
+
+    // LEVELS:
+    // TODO: Create a level prefab
+    // TODO: Make the prefab read from the current level in the levelmanager
+    // TODO: Create prefabs for room exit types (4-way, 3-way, 2-way, deadend)
+    // TODO: Create prefab variants of the base level for each level
+    // TODO: Update base game scene to contain all level prefabs
+    // TODO: When changing levels, move player and camera to the starting position for that level prefab
 }
