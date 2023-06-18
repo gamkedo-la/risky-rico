a0b1ac0563e1bbb1b1b6a38b9beb65127f4dcf7e
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-06-18 17:33:38 -0500

    spawning floating text objects for visual feedback on tutorial prompts

--- Assets/Scripts/TutorialSequence.cs
+++ Assets/Scripts/TutorialSequence.cs
@@ -10,6 +10,8 @@ public class TutorialSequence : MonoBehaviour
     [SerializeField] private List<TutorialPrompt> _prompts = new List<TutorialPrompt>();
     [SerializeField] private TMP_Text _promptText;
     [SerializeField] private TMP_Text _promptCountText;
+    [SerializeField] private GameObject _floatTextPrefab;
+
     private int _inputCounter = 0;
     private int _promptIndex = 0;
 
@@ -22,12 +24,18 @@ public class TutorialSequence : MonoBehaviour
     void Update()
     {
         TutorialPrompt prompt = CurrentPrompt();
-        
+
         if (_playerInput.actions[prompt.InputKey].triggered && _inputCounter < prompt.MaxInputCounter)
         {
+            // update the text information and play a success sound
             _inputCounter++;        
             _promptCountText.text = _inputCounter + "/" + prompt.MaxInputCounter;
             ServiceLocator.Instance.Get<AudioManager>().PlaySoundFromDictionary("BonusPoints");
+
+            // spawn text object to show a success message
+            GameObject floatText = Instantiate(_floatTextPrefab, transform.position, transform.rotation);
+            floatText.GetComponent<TMP_Text>().text = "" + _inputCounter + "/" + prompt.MaxInputCounter + "!";
+            floatText.transform.SetParent(transform.parent);
         }
 
         if (_inputCounter >= prompt.MaxInputCounter)
