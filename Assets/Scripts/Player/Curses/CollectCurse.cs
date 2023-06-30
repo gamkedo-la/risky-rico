6e3566c988fff5c8c3b02a7cb391ef77cca101ab
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-06-29 22:21:34 -0500

    fixing double shot curse

--- Assets/Scripts/Player/Curses/CollectCurse.cs
+++ Assets/Scripts/Player/Curses/CollectCurse.cs
@@ -20,19 +20,7 @@ public class CollectCurse : MonoBehaviour
                 _curseSlots.AddCurse(curseComponent.Parameters);
             }
 
-            // Remove all curses from the scene
-            List<GameObject> tempCollection = new List<GameObject>();
-            foreach(GameObject curse in _curseCollection)
-            {
-                tempCollection.Add(curse);
-            }
-
-            foreach(GameObject curse in tempCollection)
-            {
-                Destroy(curse);
-            }
-            
-            tempCollection.Clear();
+            Destroy(collision.gameObject);
         }
     }
 }
