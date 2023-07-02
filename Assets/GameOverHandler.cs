0d8fb08208205d6d4e8722783892e3394a08dd02
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-01 22:03:50 -0500

    slow mo effect on game over

---
+++ Assets/GameOverHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverHandler : MonoBehaviour
+{   
+
+    [SerializeField] private float _slowDownRate = 0.01f;
+    [SerializeField] private float _gameOverTimer;
+    private bool _sequenceStarted = false;
+
+    public void StartGameOverSequence()
+    {
+        _sequenceStarted = true;
+    }
+    
+    void Update()
+    {
+        if (_sequenceStarted)
+        {
+            // lerp slow-mo
+            Time.timeScale = Mathf.Lerp(Time.timeScale, 0f, _slowDownRate);
+        }
+
+        // scene change on countdown finish
+        if (Time.timeScale <= 0.02f)
+        {
+            _gameOverTimer -= Time.unscaledDeltaTime;
+            if (_gameOverTimer <= 0f)
+            {
+                Time.timeScale = 1f;
+                SceneManager.LoadScene("gameover");
+                _sequenceStarted = false;
+            }
+        }
+    }
+}
