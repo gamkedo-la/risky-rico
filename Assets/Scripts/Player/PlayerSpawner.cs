25fd6081bb2463716ccc748f9be9f2c0427e4592
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-15 13:08:56 -0500

    created player spawner that uses flags to determine player location

---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private GameObject _spawnPoint;
+    [SerializeField] private GameObject _spawnTarget;
+    [SerializeField] private string _spawnFlag;
+    [SerializeField] private bool _useFlags;
+
+    void Awake()
+    {
+        bool spawnFlag = ServiceLocator.Instance.Get<SaveDataManager>().GetFlag(_spawnFlag);
+        if (spawnFlag || !_useFlags)
+        {
+            Instantiate(_playerPrefab, _spawnPoint.transform.position, Quaternion.identity);
+        }
+    }
+}
