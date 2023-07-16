c0913bd2f7fdddae2a24b2f97a93cd6a5f8bb297
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-15 20:38:00 -0500

    generic SO list

---
+++ Assets/Scripts/Util/ScriptableObjectList.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableObjectList<T> : ScriptableObject
+{
+    [Tooltip("The items included in this list")]
+    [SerializeField] private List<T> _elements = new List<T>();
+    public List<T> Elements => _elements;
+
+    [Tooltip("The maximum number of elements allowed in this list")]
+    [SerializeField] int _maxLength = 0;
+    public int MaxLength => _maxLength;
+
+    public delegate void AddElement(T element);
+    public event AddElement OnAddElement;
+
+    public void Add(T element)
+    {
+        if (_elements.Count < _maxLength && !_elements.Contains(element))
+        {
+            _elements.Add(element);
+            OnAddElement(element);
+        }
+    }
+
+    public void Remove(T element)
+    {
+        if (_elements.Count > 0 && _elements.Contains(element))
+        {
+            _elements.Remove(element);
+        }
+    }
+
+    public void Clear()
+    {
+        _elements.Clear();
+    }
+}
