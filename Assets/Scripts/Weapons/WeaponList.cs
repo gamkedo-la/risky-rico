66160cb73703ce6e7726743257d7c3d591b77b15
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-15 20:38:24 -0500

    SO list based on weapon data; list of acquired weapons

---
+++ Assets/Scripts/Weapons/WeaponList.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponList", menuName = "Weapon/Weapon List", order = 1)]
+public class WeaponList : ScriptableObjectList<WeaponData>
+{
+
+}
