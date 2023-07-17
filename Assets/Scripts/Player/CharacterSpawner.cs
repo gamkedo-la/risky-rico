b3f34a30e54010d27225289a537d9e3af8c35880
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-07-16 21:17:04 -0500

    flagged spawners in graveyard scene for character introductions

--- Assets/Scripts/Player/CharacterSpawner.cs
+++ Assets/Scripts/Player/CharacterSpawner.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpawner : MonoBehaviour
+public class CharacterSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private GameObject _prefab;
     [SerializeField] private GameObject _spawnPoint;
     [SerializeField] private string _spawnFlag;
     [SerializeField] private bool _useFlags;
+    [SerializeField] private bool _flagValueToCheck;
 
     void Awake()
     {
         bool spawnFlag = ServiceLocator.Instance.Get<SaveDataManager>().GetFlag(_spawnFlag);
-        if (spawnFlag || !_useFlags)
+        if (spawnFlag == _flagValueToCheck || !_useFlags)
         {
-            Instantiate(_playerPrefab, _spawnPoint.transform.position, Quaternion.identity);
+            GameObject spawnedObject = Instantiate(_prefab, _spawnPoint.transform.position, Quaternion.identity);
+            spawnedObject.SetActive(true);
         }
     }
 }
